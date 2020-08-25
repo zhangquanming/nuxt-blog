94ff023163aead56ebcd9310a9a01280ff4f063f
zhangquanming <419654548@qq.com>
2020-08-25 20:46:27 +0800

    z

---
+++ components/framework/UserBrief/UserBrief.vue
@@ -0,0 +1,72 @@
+<template>
+  <Card :padding="0">
+    <div class="card-brief-blog-wrap">
+      <div :style="{ 'background-image': 'url(' + avatar + ')' }" class="brief-poster"></div>
+      <div class="author-avatar">
+        <div :style="{ 'background-image': 'url(' + avatar + ')' }" class="author-avatar-box"></div>
+      </div>
+      <h3 class="brief-title">{{ userInfo ? userInfo.nicName || userInfo.userName : '' }}</h3>
+    </div>
+  </Card>
+</template>
+
+<script>
+import Card from '@/components/base/Card/Card'
+import defaultAvatar from '@/assets/images/no-data-bg-colorful.png'
+
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'CardBriefBlog',
+  components: {
+    Card
+  },
+  computed: {
+    ...mapGetters(['userInfo']),
+    avatar() {
+      return this.userInfo && this.userInfo.avatar ? this.userInfo.avatar + '?x-oss-process=image/resize,m_fill,h_110,w_110' : defaultAvatar
+
+      // return this.userInfo ? this.userInfo.avatar : '';
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.card-brief-blog-wrap {
+  text-align: center;
+  .brief-poster {
+    padding-top: 61.8%;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    background-color: #d51f45;
+  }
+  .author-avatar {
+    position: relative;
+    padding-top: 38.2%;
+    width: 38.2%;
+    margin: -19% auto 0;
+    border-radius: 100%;
+    border: 5px solid @colorBorderLight;
+    box-sizing: content-box;
+  }
+  .author-avatar-box {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    border-radius: 100%;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+  .brief-title {
+    margin-top: 15px;
+    padding-bottom: 10px;
+    color: @colorTextTitle;
+    font-weight: normal;
+  }
+}
+</style>
---
+++ layouts/user.vue
@@ -0,0 +1,55 @@
+<template>
+  <app-layout>
+    <div class="z-container">
+      <div class="user-page-wrap">
+        <div class="user-page-side">
+          <div class="hidden-xs">
+            <user-brief />
+          </div>
+          <card :padding="0" style="margin-bottom: 0;">
+            <user-menu></user-menu>
+          </card>
+        </div>
+        <div class="user-page-main">
+          <nuxt />
+        </div>
+      </div>
+    </div>
+  </app-layout>
+</template>
+
+<script>
+import AppLayout from '@/components/framework/AppLayout/AppLayout'
+
+import Card from '@/components/base/Card/Card'
+import UserMenu from '@/components/framework/UserMenu/UserMenu'
+import UserBrief from '@/components/framework/UserBrief/UserBrief'
+
+export default {
+  name: 'AppLayoutUser',
+  components: {
+    AppLayout,
+    Card,
+    UserMenu,
+    UserBrief
+  },
+  head() {
+    return {
+      title: '个人中心'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.user-page-wrap {
+  display: flex;
+}
+.user-page-side {
+  width: 240px;
+  margin-right: 20px;
+}
+.user-page-main {
+  flex: 1;
+}
+</style>
